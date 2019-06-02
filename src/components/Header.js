b7b5f5671785ab1a32cc1a6119de8268f16c2ff0
Jonathan Rafael Guzman <guzman.jonathan.r@gmail.com>
2019-06-01 20:48:08 -0400

    added link to portfolio site

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Header = (props) => (
     <div className='header'>
+      <a style={{textDecoration: "none", color: "grey"}} href="http://jonathan-guzman.s3-website-us-east-1.amazonaws.com">Back To Jona's Portfolio</a>
       <div className="container">
       <h1 className='header__title'>{props.title}</h1>
       {props.subtitle && <h2 className='header__subtitle'>{props.subtitle}</h2>}
